0590992654c0676f6e49f82917da0784facfb53e
unknown <bogdan.titsky@gmail.com>
2023-05-20 09:58:10 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import './css/style.css';
 import Notiflix from 'notiflix';
 import axios from 'axios';
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const KEY = 'key=36489495-4c4bba821fe5c27fc94c3b861';
 const PER_PAGE = 40;
 let currentPage = 1;
+const initialPage = 1;
 
 const searchForm = document.querySelector('#search-form');
 const searchQuery = document.querySelector('[name="searchQuery"]');
@@ -18,23 +21,27 @@ async function clickSubmit(e) {
   e.preventDefault();
 
   const query = searchQuery.value;
+  currentPage = initialPage;
 
   try {
     const resp = await axios.get(
-      `https://pixabay.com/api/?${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
+      `https://pixabay.com/api/?${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=${PER_PAGE}`
     );
 
     const imagesArr = resp.data.hits;
 
     if (imagesArr.length === 0) {
       gallery.innerHTML = '';
-      Notiflix.Notify.info(
+      Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
     }
+    const totalHits = resp.data.totalHits;
 
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
     gallery.innerHTML = makeMarkup(imagesArr);
+    initializeLightbox();
   } catch (error) {
     console.error(error);
   }
@@ -42,13 +49,61 @@ async function clickSubmit(e) {
   btnShowMore.classList.remove('hidden');
 }
 
-async function clickShowMore(e) {}
+async function clickShowMore(e) {
+  e.preventDefault();
+  const query = searchQuery.value;
+  currentPage++;
+
+  try {
+    const resp = await axios.get(
+      `https://pixabay.com/api/?${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=${PER_PAGE}`
+    );
+
+    const totalHits = resp.data.totalHits;
+    console.log(totalHits);
+
+    const imagesArr = resp.data.hits;
+
+    if (imagesArr.length === 0) {
+      btnShowMore.classList.add('hidden');
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+
+    gallery.insertAdjacentHTML('beforeend', makeMarkup(imagesArr));
+    initializeLightbox();
+
+    const totalPages = Math.ceil(totalHits / PER_PAGE);
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+
+    if (currentPage >= totalPages) {
+      btnShowMore.classList.add('hidden');
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    } else {
+      btnShowMore.classList.remove('hidden');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 function makeMarkup(arr) {
   return arr
     .map(
       ({ webformatURL, tags, likes, views, comments, downloads }) => `
-      <div class="photo-card">
+      <a href="${webformatURL}" class="photo-card">
         <img src="${webformatURL}" alt="${tags}" loading="lazy" />
         <div class="info">
           <p class="info-item">
@@ -64,9 +119,11 @@ function makeMarkup(arr) {
             <b>Downloads: ${downloads}</b>
           </p>
         </div>
-      </div>`
+      </a>`
     )
     .join('');
 }
 
-// https://pixabay.com/api/?${KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true
+function initializeLightbox() {
+  const lightbox = new simpleLightbox('.gallery a');
+}
